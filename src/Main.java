7c4ed0e46825e9089172172476d90f4c02db40ad
Camilo_WH <yefersoncamiloblanco@gmail.com>
2025-08-29 17:30:25 -0500

    comandos git

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hola Mundo");
+        System.out.println("git config -list");
+        System.out.println("git clone");
+        System.out.println("add");
+        System.out.println("git branch -d rama");
+        System.out.println("git commit -m");
+        System.out.println("git push origin rama");
+        System.out.println("git branch");
     }
 }
